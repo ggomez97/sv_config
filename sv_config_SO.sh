4a4bcf79f3a1fa916704b5f1fa318452c11d4715
Gabriel <40162276+ggomez97@users.noreply.github.com>
2018-07-29 16:13:47 -0300

    ngm

--- sv_config_SO.sh
+++ sv_config_SO.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 su -
+
 hostnamectl set-hostname servidor.its 
 
 cat /sv_config/network_config.txt > /etc/sysconfig/network-scripts/ifcfg-enp0s3
 
-
+systemctl stop NetworkManager
+systemctl disable NetworkManager
+systemctl restar network.service
+ip add show
 
 
 
