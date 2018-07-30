14a3a05e3eada7d90382a4c4d7530c54bcb1fe60
Gabriel <40162276+ggomez97@users.noreply.github.com>
2018-07-30 04:07:01 -0300

    Gabo

--- sv_config_SO.sh
+++ sv_config_SO.sh
@@ -12,4 +12,21 @@ cat /etc/sysconfig/network-scripts/ifcfg-enp0s3
 
 hostnamectl 
 
+read -n1
+
+groupadd informix                                    # Crear un grupo llamado informix
+useradd -g informix -d /dev/null informix            # Crear usuario informix y lo agrega al grupo informix
+passwd informix                                      # Asigna una contraseña del usuario informix
+mkdir /opt/informix                                  # Crear el directorio de informix
+chown informix.informix /opt/informix                # Le damos la propiedad a informix 
+cat config_informix.txt > /etc/profile.d/informix.sh 
+
+mv informix_comprimido.tar /opt/informix
+cd /opt/informix
+su informix -c "tar -xvf informix_comprimido.tar"
+./ids_install
+touch /opt/informix/etc/sqlhosts
+chown informix.informix /opt/informix/etc/sqlhosts
+cat sqlhosts.txt > /opt/informix/etc/sqlhosts
+
 
