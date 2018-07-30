8d2fb93d0a8cc2ea241c9922ecceb5715b29630b
Gabriel <40162276+ggomez97@users.noreply.github.com>
2018-07-30 18:06:28 -0300

    sdas

--- sv_config_SO.sh
+++ sv_config_SO.sh
@@ -25,8 +25,18 @@ mv informix_comprimido.tar /opt/informix
 cd /opt/informix
 su informix -c "tar -xvf informix_comprimido.tar"
 ./ids_install
+
 touch /opt/informix/etc/sqlhosts
 chown informix.informix /opt/informix/etc/sqlhosts
 cat sqlhosts.txt > /opt/informix/etc/sqlhosts
+cat onconfig.txt > /opt/informix/etc/onconfig.std # Cambia LTAPEDEV /dev/null , ROOTPATH /opt/informix/logdir/rootdbs
+                                                    # DBSERVERNAME miServidor
 
+su informix -c "mkdir /opt/informix/logdir"               # Crea el directorio /opt/informix/logdir	
+chown informix.informix /opt/informix/logdir         # Crea el directorio /opt/informix/logdir	
+informix -c "chmod 770 /opt/informix/logdir"           # Modificamos los permisos
+su informix -c "touch /opt/informix/logdir/rootdbs"       # Creamos el archivo rootdbs 
+su informix -c "chmod 660 /opt/informix/logdir/rootdbs"   # Modificamos los permisos al dbspace root
+sudo chown informix.informix /opt/informix/logdir/rootdbs # Modificamos el propietario al dbspace root
 
+cat services.txt > /etc/service 
