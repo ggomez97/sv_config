aec32bf63a8a382a2a3127c1d6881435b07993ff
Gabriel <40162276+ggomez97@users.noreply.github.com>
2018-08-01 02:16:07 -0300

    123

--- informix_automatico1.sh
+++ informix_automatico1.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
     clear 
 
-mkdir /opt/informix                                  # Crear el directorio de informix
-chown informix.informix /opt/informix                # Le damos la propiedad a informix 
+sudo mkdir /opt/informix                                  # Crear el directorio de informix
+sudo chown informix.informix /opt/informix                # Le damos la propiedad a informix 
 sudo cat config_informix.txt > /etc/profile.d/informix.sh 
 
 cat /etc/profile.d/informix.sh 
+
+read -n1
+
 sudo mv informix_comprimido.tar /opt/informix
 cd /opt/informix
 su informix -c "tar -xvf informix_comprimido.tar"
--- sv_config_SO.sh
+++ sv_config_SO.sh
@@ -14,9 +14,9 @@ hostnamectl
 
 read -n1
 
-groupadd informix                                    # Crear un grupo llamado informix
-useradd -g informix -d /dev/null informix            # Crear usuario informix y lo agrega al grupo informix
-passwd informix                                      # Asigna una contraseña del usuario informix
+sudo groupadd informix                                    # Crear un grupo llamado informix
+sudo useradd -g informix -d /home/informix informix            # Crear usuario informix y lo agrega al grupo informix
+sudo passwd informix                                      # Asigna una contraseña del usuario informix
 
 
 read -n1
