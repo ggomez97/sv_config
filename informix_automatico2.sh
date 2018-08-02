6eaba96b5f87b3130fe8b1e8208c14e6c19442f8
Gabriel <40162276+ggomez97@users.noreply.github.com>
2018-08-02 01:05:11 -0300

    dcsa

--- informix_automatico2.sh
+++ informix_automatico2.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
     clear
 
-sudo touch /opt/informix/etc/sqlhosts
-sudo chown informix.informix /opt/informix/etc/sqlhosts
+touch /opt/informix/etc/sqlhosts
+chown informix.informix /opt/informix/etc/sqlhosts
 su informix -c "cat sqlhosts.txt > /opt/informix/etc/sqlhosts"
 su informix -c "cat onconfig.txt > /opt/informix/etc/onconfig.std" # Cambia LTAPEDEV /dev/null , ROOTPATH /opt/informix/logdir/rootdbs
                                                     # DBSERVERNAME miServidor
 
 su informix -c "mkdir /opt/informix/logdir"               # Crea el directorio /opt/informix/logdir	
-sudo chown informix.informix /opt/informix/logdir         # Crea el directorio /opt/informix/logdir	
+chown informix.informix /opt/informix/logdir         # Crea el directorio /opt/informix/logdir	
 su informix -c "chmod 770 /opt/informix/logdir"           # Modificamos los permisos
 su informix -c "touch /opt/informix/logdir/rootdbs"       # Creamos el archivo rootdbs 
 su informix -c "chmod 660 /opt/informix/logdir/rootdbs"   # Modificamos los permisos al dbspace root
-sudo chown informix.informix /opt/informix/logdir/rootdbs # Modificamos el propietario al dbspace root
+chown informix.informix /opt/informix/logdir/rootdbs # Modificamos el propietario al dbspace root
 
-sudo cat services.txt > /etc/service 
-sudo cat /etc/service 
-
-read -n1
-
-sudo firewall-cmd --zone=public --permanent --add-port=50000/tcp
-sudo firewall-cmd --reload
+cat services.txt > /etc/service 
+cat /etc/service 
 
 read -n1
 
-sudo mkdir /mis_dbspaces
-sudo chown informix.informix /mis_dbspaces # Modificamos los permisos
+firewall-cmd --zone=public --permanent --add-port=50000/tcp
+firewall-cmd --reload
+
+read -n1
+
+mkdir /mis_dbspaces
+chown informix.informix /mis_dbspaces # Modificamos los permisos
 
 su -
 source /etc/profile.d/informix.sh
