c03ec3c4fa71780a3744f59f018f7fa64ef8e6a4
Gabriel <40162276+ggomez97@users.noreply.github.com>
2018-08-02 01:07:44 -0300

    dsa

--- sv_config_SO.sh
+++ sv_config_SO.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
     clear
-
 hostnamectl set-hostname servidor.its 
 
 cd /etc/sysconfig/network-scripts
@@ -9,9 +8,8 @@ echo 'IPADDR="192.168.10.141"' >> ifcfg-enp0s3
 echo 'PREFIX="24"' >> ifcfg-enp0s3
 echo 'GATEWAY="192.168.10.1"' >> ifcfg-enp0s3
 echo 'IPV6_PRIVACY="no"' >> ifcfg-enp0s3
-echo 'ZONE=public"' >> ifcfg-enp0s3
-
-sed -E -i 's/(BOOTPROTO=")([a-zA-Z]*)(")\1/none\3/g' ifcfg-enp0s3
+echo 'ZONE="public"' >> ifcfg-enp0s3
+sed -E -i 's/(BOOTPROTO=)([a-zA-Z]*)/\1none/g' ifcfg-enp0s3
 
 systemctl stop NetworkManager
 systemctl disable NetworkManager
@@ -19,12 +17,21 @@ systemctl restart network.service
 
 hostnamectl 
 
-#read -n1
 
-#sudo groupadd informix                                    # Crear un grupo llamado informix
-#sudo useradd -g informix -d /home/informix informix            # Crear usuario informix y lo agrega al grupo informix
-#sudo passwd informix                                      # Asigna una contraseña del usuario informix
+read -n1
+
+groupadd informix                                    # Crear un grupo llamado informix
+useradd -g informix -d /home/informix informix            # Crear usuario informix y lo agrega al grupo informix
+passwd informix                                      # Asigna una contraseña del usuario informix
 
 
-#read -n1
+
+#groupadd invitado
+
+#groupadd docente
+
+#groupadd gestion
+
+#groupadd administrador
+
 
