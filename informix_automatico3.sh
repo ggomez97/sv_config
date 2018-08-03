0adb0ae79e47596519b44fad415f1832b0bcc837
Gabriel <40162276+ggomez97@users.noreply.github.com>
2018-08-03 02:33:17 -0300

    finished

--- informix_automatico3.sh
+++ informix_automatico3.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 clear
-
 oninit -ivy 
 onstat -
 
@@ -8,7 +7,7 @@ read -n1
 
 cd /mis_dbspaces
 touch ITS_dbspace.dbspace
-chown informix.informix ITS_dbspace.dbspace # Modificamos los permisos
+chown informix.informix ITS_dbspace.dbspace 
 chmod 660 ITS_dbspace.dbspace
 
 onspaces -c -d ITS_dbspace -p /mis_dbspaces/ITS_dbspace.dbspace -o 0 -s 1000000 
