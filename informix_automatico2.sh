25404de2eccbda086a4e524c044d7efe443a9b45
Gabriel <40162276+ggomez97@users.noreply.github.com>
2018-08-03 02:32:58 -0300

    Finished

--- informix_automatico2.sh
+++ informix_automatico2.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
     clear
 
-sudo touch /opt/informix/etc/sqlhosts
-sudo chown informix.informix /opt/informix/etc/sqlhosts
-sudo su informix -c "cat sqlhosts.txt > /opt/informix/etc/sqlhosts"
-sudo su informix -c "cat onconfig.txt > /opt/informix/etc/onconfig.std" # Cambia LTAPEDEV /dev/null , ROOTPATH /opt/informix/logdir/rootdbs
+touch /opt/informix/etc/sqlhosts
+chown informix.informix /opt/informix/etc/sqlhosts
+su informix -c "cat sqlhosts.txt > /opt/informix/etc/sqlhosts"
+su informix -c "cat onconfig.txt > /opt/informix/etc/onconfig.std" # Cambia LTAPEDEV /dev/null , ROOTPATH /opt/informix/logdir/rootdbs
                                                     # DBSERVERNAME miServidor
 
-sudo su informix -c "mkdir /opt/informix/logdir"               # Crea el directorio /opt/informix/logdir	
-sudo chown informix.informix /opt/informix/logdir         # Crea el directorio /opt/informix/logdir	
-sudo su informix -c "chmod 770 /opt/informix/logdir"           # Modificamos los permisos
-sudo su informix -c "touch /opt/informix/logdir/rootdbs"       # Creamos el archivo rootdbs 
-sudo su informix -c "chmod 660 /opt/informix/logdir/rootdbs"   # Modificamos los permisos al dbspace root
-sudo chown informix.informix /opt/informix/logdir/rootdbs # Modificamos el propietario al dbspace root
+su informix -c "mkdir /opt/informix/logdir"               # Crea el directorio /opt/informix/logdir	
+chown informix.informix /opt/informix/logdir         # Crea el directorio /opt/informix/logdir	
+su informix -c "chmod 770 /opt/informix/logdir"           # Modificamos los permisos
+su informix -c "touch /opt/informix/logdir/rootdbs"       # Creamos el archivo rootdbs 
+su informix -c "chmod 660 /opt/informix/logdir/rootdbs"   # Modificamos los permisos al dbspace root
+chown informix.informix /opt/informix/logdir/rootdbs # Modificamos el propietario al dbspace root
 
-sudo echo 'informix	    50000/tcp		        # Informix server' >> /etc/services
+cat services.txt > /etc/services
 
 read -n1
 
@@ -25,18 +25,3 @@ read -n1
 
 mkdir /mis_dbspaces
 chown informix.informix /mis_dbspaces # Modificamos los permisos
-
-su -
-
-oninit -ivy 
-onstat -
-
-read -n1
-
-cd /mis_dbspaces
-sudo touch ITS_dbspace.dbspace
-chown informix.informix ITS_dbspace.dbspace # Modificamos los permisos
-chmod 660 ITS_dbspace.dbspace
-
-onspaces -c -d ITS_dbspace -p /mis_dbspaces/ITS_dbspace.dbspace -o 0 -s 1000000 
-
