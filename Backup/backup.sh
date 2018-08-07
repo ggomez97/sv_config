f6bd7098a2bd1af6196b180969c440c8b3fd973d
Gabriel <40162276+ggomez97@users.noreply.github.com>
2018-08-07 18:06:25 -0300

    backup

---
+++ Backup/backup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ clear
+
+ fecha='date +%Y-%m-%d'
+  echo $fecha
+if [ ! -d /home/gservidor/log_bk  ]; then 
+	mkdir /home/gservidor/log_bk 
+fi
+
+ read -n1
+
+ rm -rf /home/gservidor/log_bk-/backup$fecha
+ echo "Se esta haciendo el nuevo vackup para el dia $fecha"
+ mkdir /home/gservidor/log_bk/backup$fecha
+ mkdir /home/gservidor/log_bk/backup$fecha/db /home/gservidor/log_bk/backup$fecha/sistema
+ dbexport proyecto_x -o /home/gservidor/log_bk/backup$fecha/db -ss 
+ cp /etc/passwd /home/gservidor/backup$fecha/sistema
+
+ echo "Se realizo el backup"
+ read -n1
+
+
