4f8702780b23b586a560b81bb17f6f5622f0c1f0
Gabriel <40162276+ggomez97@users.noreply.github.com>
2018-08-07 21:52:29 -0300

    se

--- Backup/backup.sh
+++ Backup/backup.sh
@@ -9,12 +9,14 @@ fi
 
  read -n1
 
- rm -rf /home/gservidor/log_bk-/backup$fecha
- echo "Se esta haciendo el nuevo vackup para el dia $fecha"
- mkdir /home/gservidor/log_bk/backup$fecha
- mkdir /home/gservidor/log_bk/backup$fecha/db /home/gservidor/log_bk/backup$fecha/sistema
- dbexport proyecto_x -o /home/gservidor/log_bk/backup$fecha/db -ss 
- cp /etc/passwd /home/gservidor/backup$fecha/sistema
+rm -rf /home/gservidor/log_bk-/backup$fecha
+echo "Se esta haciendo el nuevo vackup para el dia $fecha"
+mkdir /home/gservidor/log_bk/backup$fecha
+mkdir /home/gservidor/log_bk/backup$fecha/db /home/gservidor/log_bk/backup$fecha/sistema
+dbexport proyecto_x -o /home/gservidor/log_bk/backup$fecha/db -ss 
+cp /etc/passwd /home/gservidor/backup$fecha/sistema
+cp /etc/sysconfig/network-scripts/ifcfg-enp0s3 /home/gservidor/log_bk/backup$fecha/sistema
+cp /etc/services /home/gservidor/log_bk/backup$fecha/sistema
 
  echo "Se realizo el backup"
  read -n1
