d676ab4125dd7933a3038a849fb5c85dbf40ac39
Gabriel <40162276+ggomez97@users.noreply.github.com>
2018-08-07 23:47:24 -0300

    p

--- informix_automatico3.sh
+++ informix_automatico3.sh
@@ -12,3 +12,8 @@ chown informix.informix ITS_dbspace.dbspace
 chmod 660 ITS_dbspace.dbspace
 
 onspaces -c -d ITS_dbspace -p /mis_dbspaces/ITS_dbspace.dbspace -o 0 -s 1000000 
+
+if [ ! -d /opt/informix/backup ]; then 
+ su informix -c "mkdir /opt/informix/backup"
+fi
+chmod 770 /opt/informix/backup
