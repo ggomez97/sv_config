3bac7d82ead2b9337eedfe599fd7389274d9b976
Gabriel <40162276+ggomez97@users.noreply.github.com>
2018-08-07 21:53:09 -0300

    CC

--- Backup/backup.sh
+++ Backup/backup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
  clear
 
- fecha='date +%Y-%m-%d'
+ fecha=`date +%Y-%m-%d`
   echo $fecha
+ usuario=`whoami`
 if [ ! -d /home/gservidor/log_bk  ]; then 
 	mkdir /home/gservidor/log_bk 
 fi
@@ -18,7 +19,11 @@ cp /etc/passwd /home/gservidor/backup$fecha/sistema
 cp /etc/sysconfig/network-scripts/ifcfg-enp0s3 /home/gservidor/log_bk/backup$fecha/sistema
 cp /etc/services /home/gservidor/log_bk/backup$fecha/sistema
 
- echo "Se realizo el backup"
- read -n1
+cd /home/gservidor/log_bk
 
+tar -cvjf backup$fecha.tar backup$fecha
+ echo "Se realizo el backup"
+
+ read -n1
+cd $HOME
 
---
+++ Backup/old_bk.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+clear
+    FECHA=`date +%Y-%m-%d -d "-8 days"`
+read -n1
+    cd /home/gservidor/log_bk
+
+if [ -e /home/gservidor/log_bk/backup$FECHA.tar];then
+
+echo "Borrando el archivo que se encuentra en /home/gservidor/log_bk/backup$FECHA.tar"
+	read -n1
+	rm -rf /home/gservidor/log_bk/backup$FECHA.tar
+fi
+cd $HOME
