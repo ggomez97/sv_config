217ae3a84334a86342e8e4f3948c9d01cd394b8d
Gabriel <40162276+ggomez97@users.noreply.github.com>
2018-09-24 16:52:31 -0300

    Falta informix/backup

--- Scripts_Menu/backup.sh
+++ Scripts_Menu/backup.sh
@@ -4,34 +4,36 @@
  fecha=`date +%Y-%m-%d`
   echo $fecha
  usuario=`whoami`
-if [ ! -d /home/gservidor/log_bk  ]; then 
-	mkdir /home/gservidor/log_bk 
-fi
+if [ ! -d /home/$usuario/log_bk  ]; then 
+	sudo mkdir /home/$usuario/log_bk 
+fi 
 
 read -n1
 
-rm -rf /home/gservidor/log_bk-/backup$fecha
+sudo rm -rf /home/$usuario/log_bk-/backup$fecha
 echo "Se esta haciendo el nuevo backup para el dia $fecha"
-sudo mkdir /home/gservidor/log_bk/backup$fecha
-sudo mkdir /home/gservidor/log_bk/backup$fecha/db /home/gservidor/log_bk/backup$fecha/sistema
+sudo mkdir /home/$usuario/log_bk/backup$fecha
+sudo mkdir /home/$usuario/log_bk/backup$fecha/db /home/$usuario/log_bk/backup$fecha/sistema
 
-#dbexport gestion_utu -o /home/gservidor/log_bk/backup$fecha/db -ss 
+dbexport gestion_utu -o /home/$usuario/log_bk/backup$fecha/db -ss 
 
-sudo cp /etc/passwd /home/gservidor/backup$fecha/sistema
+sudo cp /etc/passwd /home/$usuario/backup$fecha/sistema
 
-sudo cp /etc/sysconfig/network-scripts/ifcfg-enp0s3 /home/gservidor/log_bk/backup$fecha/sistema
+sudo cp /etc/sysconfig/network-scripts/ifcfg-enp0s3 /home/$usuario/log_bk/backup$fecha/sistema
 
-sudo cp /etc/services /home/gservidor/log_bk/backup$fecha/sistema
+sudo cp /etc/services /home/$usuario/log_bk/backup$fecha/sistema
 
-sudo cp /var/log/secure /home/gservidor/log_bk/backup$fecha/sistema
+sudo cp /var/log/secure /home/$usuario/log_bk/backup$fecha/sistema
 
-sudo cp /var/log/messages /home/gservidor/log_bk/backup$fecha/sistema
+sudo cp /var/log/messages /home/$usuario/log_bk/backup$fecha/sistema
 
-#sudo cp /opt/informix/backup /home/gservidor/log_bk/backup$fecha/sistema
+sudo cp /opt/informix/backup /home/$usuario/log_bk/backup$fecha/sistema
 
-cd /home/gservidor/log_bk
+cd /home/$usuario/log_bk
 
-tar -cvjf backup$fecha.tar backup$fecha
+sudo tar -cvjf backup$fecha.tar backup$fecha
+
+
  echo "Se realizo el backup"
 
  read -n1
